e3877b962648bc256e3468436861949c8de3ee34
swarnim151 <43801043+swarnim151@users.noreply.github.com>
2020-04-22 18:23:27 -0700

    Added profile page with grids of Kricheso

--- src/grid_generator.js
+++ src/grid_generator.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Firestore from "./services/firestore";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -18,44 +18,51 @@ const useStyles = makeStyles({
   },
 });
 
-export default function MiaCard() {
+export default function CreatedGrid() {
   const classes = useStyles();
+  const [username, setUsername] = useState("");
+  const [grid_list, setgridlist] = useState([]);
 
-  async function print_func() {
-    const grid_list = await Firestore.get.gridsCreatedByUser(
+  useEffect(load_grid, []);
+
+  async function load_grid() {
+    const firestore_grid = await Firestore.get.gridsCreatedByUser(
       "kricheso@google.com",
       "kricheso@google.com"
     );
-    console.log(grid_list[0]);
 
-    return grid_list[0];
+    setgridlist(firestore_grid);
   }
-  return (
-    <div>
-      {print_func().id}
-      fbsffs
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia className={classes.media} />
 
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Gridlock
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Kousei's course 2
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Play
-          </Button>
-          <Button size="small" color="primary">
-            Delete
-          </Button>
-        </CardActions>
-      </Card>
+  return (
+    <div className="board">
+      {grid_list.map((grid, i) => {
+        return (
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia className={classes.media} />
+
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Gridlock
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {grid.id}
+                  {grid.title}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Play
+              </Button>
+              <Button size="small" color="primary">
+                Delete
+              </Button>
+            </CardActions>
+          </Card>
+        );
+      })}
     </div>
   );
 }
