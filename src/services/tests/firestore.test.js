9f187a07c0e62c3183c24d4d3672c59d1ad7a3d2
Kousei Richeson <kousei@nmsu.edu>
2020-04-24 10:09:49 -0700

    Combined Test Cases and Reversed a Change to the README

--- src/services/tests/firestore.test.js
+++ src/services/tests/firestore.test.js
@@ -18,60 +18,61 @@ test('Sanity Check', () => {
     expect("hi").toBe("hi");
 });
 
-test('Trying to add an invalid grid', async function () {
+test('Like and Unlike Grids', async function () {
+    
+    // invalid grid
     expect(await Firestore.add.grid(testUser, testGridTitle, [["S", "0"], ["F", "F"]])).toBeFalsy();
-});
 
-test('Adding a valid grid', async function () {
+    // grid created
     expect(await Firestore.add.grid(testUser, testGridTitle, [["S", "0"], ["1", "F"]])).toBeTruthy();
-});
 
-test('Adding a valid grid but a duplicate title', async function () {
-    expect(await Firestore.add.grid(testUser, testGridTitle, [["S", "0"], ["1", "F"]])).toBeFalsy();
-});
+    // duplicate title
+    expect(await Firestore.add.grid(testUser, testGridTitle, [["S", "0"], ["1", "F"]])).toBeFalsy(); 
+    
+    // grid liked
+    expect(await Firestore.add.like(testUser, testGrid)).toBeTruthy(); 
 
-test('Liking a grid', async function () {
-    expect(await Firestore.add.like(testUser, testGrid)).toBeTruthy();
-});
-
-test('Trying to like an already liked grid', async function () {
+    // grid already liked
     expect(await Firestore.add.like(testUser, testGrid)).toBeFalsy();
-});
 
-test('Disliking a grid', async function () {
+    // grid unliked
     expect(await Firestore.remove.like(testUser, testGrid)).toBeTruthy();
-});
 
-test('Trying to dislike an already disliked grid', async function () {
+    // grid already disliked
     expect(await Firestore.remove.like(testUser, testGrid)).toBeFalsy();
+
+    // dismount subject under testing
+    expect(await Firestore.remove.grid(testGrid)).toBeTruthy();
+
 });
 
-test('Adding an incomplete score', async function () {
+test('Adding Some Scores to a Grid', async function () {
+
+     // grid created
+    expect(await Firestore.add.grid(testUser, testGridTitle, [["S", "0"], ["1", "F"]])).toBeTruthy();
+
+    // add incomplete score
     expect(await Firestore.add.incompleteScore(testUser, testGrid));
-});
-
-test('Make sure the grid is updating statistics for incomplete scores', async function () {
-    const grid = await Firestore.get.gridForUnregisteredUser(testGrid);
+    
+    // check statistics
+    let grid = await Firestore.get.gridForUnregisteredUser(testGrid);
     expect(grid.numberOfAttempts).toBe(1);
     expect(grid.numberOfCompletes).toBe(0);
     expect(grid.numberOfIncompletes).toBe(1);
-});
 
-test('Adding a complete score', async function () {
+    // add complete score
     expect(await Firestore.add.score(testUser, testGrid, 1000));
-});
 
-test('Make sure the grid is updating statistics for complete scores', async function () {
-    const grid = await Firestore.get.gridForUnregisteredUser(testGrid);
+    // check statistics
+    grid = await Firestore.get.gridForUnregisteredUser(testGrid);
     expect(grid.numberOfAttempts).toBe(2);
     expect(grid.numberOfCompletes).toBe(1);
     expect(grid.numberOfIncompletes).toBe(1);
-});
 
-test('Dismount subject under testing', async function () {
+    // dismount subject under testing
     expect(await Firestore.remove.grid(testGrid)).toBeTruthy();
-});
 
-test('Trying to remove a non-existing grid', async function () {
+    // trying to remove a non-existing grid
     expect(await Firestore.remove.grid(testGrid)).toBeFalsy();
+
 });
