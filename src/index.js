954ed10ae81abfcff440d98077c2b290aef4815d
Kousei Richeson <kousei@nmsu.edu>
2020-04-25 17:21:51 -0700

    Added Skeleton Code for Leaderboard Component

--- src/index.js
+++ src/index.js
@@ -11,13 +11,22 @@ import Homepage from './Homepage.js';
 import Grid from "./grid_generator";
 import FirestoreManualTests from './services/tests/firestoreManualTests.js';
 import Routing from './Routing.js'
+import Leaderboard from './models/Leaderboard/Leaderboard'
+
+// Note: some score attributes are missing. Testing purposes only.
+// const fakeHiscores = [
+//   {solveTime: 5000, userDisplayName: "Kousei Richeson", userId: "kousei.richeson@google.com"}, 
+//   {solveTime: 6000, userDisplayName: "Azeezah Muhammad", userId: "azeezah@google.com"}
+// ];
 
 ReactDOM.render(
   <React.StrictMode>
     {/*/<ExplorePage />
     //<Play/>
     //<App />
-    //<FirestoreManualTests />*/}
+    //<FirestoreManualTests />
+    <Leaderboard hiscores={fakeHiscores}/>
+    */}
     <Routing />
   </React.StrictMode>,
   document.getElementById("root")
