c1b180f6f51d85dbdd03d1282bd98d2f2a9ba45e
Azeezah Muhammad <azeezahm14@gmail.com>
2020-05-01 19:39:33 -0700

    Add follow button to profile page

--- src/Profile.js
+++ src/Profile.js
@@ -24,6 +24,9 @@ const useStyles = makeStyles({
   media: {
     height: 140,
   },
+  follow: {
+    color: 'white',
+  }
 });
 
 export default function Profile(props) {
@@ -35,30 +38,24 @@ export default function Profile(props) {
   const [profilepicture, setProfilpic] = useState("");
   const [profileUser, setProfileUser] = useState(null);
   const [currentUser, setCurrentUser] = useState(null);
+  const [following, setFollowing] = useState(false);
 
   const click = () => {
     alert("click")
   }
 
-
-  useEffect(() => {
-    getCurrentUser();
-  }, []);
-
-  useEffect(() => {
-      getProfileUser();
-  }, [currentUser]);
-
+  useEffect(() => { getCurrentUser(); }, []);
+  useEffect(() => { getProfileUser(); }, [currentUser]);
 
   function displayNewUserDetails(){
   }
-
 
   async function getCurrentUser() {
     const randomGrid = await Firestore.get.trendingGridsForUnregisteredUser();
     const user = await Authentication.currentUser();
     if (user === null) { console.log("error or the user is not logged in"); return; }
     setCurrentUser(user);
+    checkFollowing(user);
   }
 
   async function getProfileUser() {
@@ -73,6 +70,40 @@ export default function Profile(props) {
     }
   }
 
+  async function checkFollowing(user) {
+    if (!profileId) { return; }
+    const followers = await Firestore.get.followers(profileId);
+    if (!followers) {
+      console.log("Couldn't load followers for");
+      console.log(profileUser);
+    }
+    if (followers.map(follower=>follower.id).includes(user.id)) {
+      setFollowing(true);
+    }
+  }
+
+  async function follow() {
+    const success = await Firestore.add.follow(currentUser.id, profileUser.id);
+    if (!success) {
+      console.log("Couldn't follow");
+      console.log(profileUser);
+      setFollowing(true);  // Probably couldn't follow because they're already following.
+    } else {
+      setFollowing(true);
+    }
+  }
+
+  async function unfollow() {
+    const success = await Firestore.remove.follow(currentUser.id, profileUser.id);
+    if (!success) {
+      console.log("Couldn't unfollow");
+      console.log(profileUser);
+      setFollowing(false);  // Probably couldn't unfollow because they're already not following.
+    } else {
+      setFollowing(false);
+    }
+  }
+
   return (
     <div className="profile">
       <div className="profileCard">
@@ -81,11 +112,16 @@ export default function Profile(props) {
           <h2> {profileUser? profileUser.displayName: " "} </h2>
           <p>
           <span>
-          <span className="star"></span>   <b>{profileUser ?  profileUser.numberOfTotalLikes: " "}</b> </span>
-          <span> <b> {profileUser? profileUser.numberOfFollowers: " "}</b> Followers </span>
+          <span className="star"></span>   <b>{profileUser ?  profileUser.numberOfTotalLikes: " "} </b> Likes </span>
+          <span> <b> {profileUser? profileUser.numberOfFollowers + (following ? 1 : 0): " "}</b> Followers </span>
           <span>  <b> {profileUser? profileUser.numberOfFollowing: " "} </b>  Following</span>
           </p>
-           <NewCard />
+    {
+      currentUser != profileUser ?
+      (following ? <Button className={classes.follow} onClick={unfollow}>Unfollow</Button>
+                 : <Button className={classes.follow} onClick={follow}>Follow</Button>)
+      : ""
+    }
         </div>
       </div>
       <ProfileCards profileId={profileId? profileId : profileUser ? profileUser.id : null}/>
